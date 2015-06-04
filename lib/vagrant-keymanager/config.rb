fd49f9b456b6f53e39f4b72191651e529b1934bf
giobaldac <giorgio.baldaccini@moviri.com>
2015-06-04 16:00:12 +0200

    .

--- lib/vagrant-keymanager/config.rb
+++ lib/vagrant-keymanager/config.rb
@@ -1,17 +1,17 @@
 module VagrantPlugins
   module KeyManager
     class Config < Vagrant.plugin('2', :config)
-      attr_accessor :extra_params
       attr_accessor :extra_steps
+      attr_accessor :user_list
 
       def initialize
-        @extra_params = []
-        @extra_params = Array.new
         @extra_steps = nil
+        @user_list = UNSET_VALUE
       end
 
       def finalize!
-        @extra_params = [ @extra_params ].flatten
+        @user_list = [] if @user_list == UNSET_VALUE
+        @user_list = [ "root", @user_list ].flatten.uniq
       end
 
       def validate(machine)
@@ -19,20 +19,19 @@ module VagrantPlugins
         # errors << validate_bool('keymanager.enabled', @enabled)
         errors.compact!
 
-        # check if extra_params option is an Array
-        if  !machine.config.keymanager.extra_params.kind_of?(Array) &&
-            !machine.config.keymanager.extra_params.kind_of?(String)
-          errors << I18n.t('vagrant_keymanager.config.not_an_array_or_string', {
-            :config_key => 'keymanager.extra_params',
-            :is_class   => extra_params.class.to_s,
-          })
-        end
-
         if !machine.config.keymanager.extra_steps.nil? &&
            !machine.config.keymanager.extra_steps.kind_of?(Proc)
           errors << I18n.t('vagrant_keymanager.config.not_a_proc', {
             :config_key => 'keymanager.extra_steps',
             :is_class   => extra_steps.class.to_s,
+          })
+        end
+
+        if  !machine.config.keymanager.user_list.kind_of?(Array) &&
+            !machine.config.keymanager.user_list.kind_of?(String)
+          errors << I18n.t('vagrant_keymanager.config.not_an_array_or_string', {
+            :config_key => 'keymanager.user_list',
+            :is_class   => user_list.class.to_s,
           })
         end
 
--- lib/vagrant-keymanager/hosts_file.rb
+++ lib/vagrant-keymanager/hosts_file.rb
@@ -2,111 +2,135 @@ require 'tempfile'
 require 'pp'
 
 module VagrantPlugins
-  module KeyManager
-    module HostsFile
-      def get_guest_keys(machine)
-        machines = get_machines
+	module KeyManager
+		module HostsFile
+			def get_guest_keys(machine)
+				machines = get_machines
+				#running_machines = machines.reject {|m| m.state.short_description.to_s != "running"}
+				running_machines = machines.select {|m| m.communicate.ready? }
 
-        sshkeys = Hash.new
-        sshrootkeys = Hash.new        
+				machines.each do |curr_machine|
+					#if curr_machine.state.short_description.to_s != "running"
+					if ! curr_machine.communicate.ready?
+						puts "Skipping machine "+curr_machine.name.to_s+ ". It's not in running state"
+					end
+				end
 
-        puts "MACHINES"
-        machines.each do |curr_machine|
-          puts curr_machine.name.to_s
-          pp curr_machine.state
-        end
+				sshkeys = Hash.new
+				required_users = machine.config.keymanager.user_list
 
-        machines.each do |curr_machine|
-            #pp curr_machine
-            curr_machine_name=curr_machine.name.to_s
-            puts "Getting SSH keys from "+curr_machine_name
-            sshkey=get_user_key(curr_machine)
-            #puts "SSH key: "+sshkey
-            sshkeys[curr_machine_name] = sshkey
-            sshrootkey=get_root_key(curr_machine)
-            #puts "SSH root key: "+sshrootkey
-            sshrootkeys[curr_machine_name] = sshrootkey
-        end
+				puts "REQUIRED USERS:"
+				pp required_users
 
-        machines.each do |curr_machine|
-          curr_machine_name=curr_machine.name.to_s
-          curr_machine.communicate.sudo("rm -f /tmp/.all_keys.txt /tmp/.all_root_keys.txt /tmp/add_ssh_keys.sh")
+				running_machines.each do |curr_machine|
+					curr_machine.communicate.sudo("rm -f /tmp/add_ssh_keys.sh /tmp/get_user_keys.sh")
+					# TODO: save these 2 bash script with curr_machine.communicate.upload
+					# We must save locally a bash script that computes and applies diff and always exits with 0 or vagrant plugin will exit with an error
+					curr_machine.communicate.execute("echo -e 'diff --changed-group-format=\"%>\" --unchanged-group-format=\"\" ~/.ssh/authorized_keys $1 >>~/.ssh/authorized_keys\nexit 0' >/tmp/add_ssh_keys.sh")
+					# We must save locally a bash script that gets ssh keys from any user (will passed as a aparameter)
+					curr_machine.communicate.execute("echo -e 'if [ ! -e ~/.ssh/id_rsa ] || [ ! -e ~/.ssh/id_rsa.pub ]; then\n\tssh-keygen -q -f ~/.ssh/id_rsa -P \"\"\nfi\ncat ~/.ssh/id_rsa.pub' >/tmp/get_user_keys.sh")
 
-          puts "Saving public SSH keys to "+curr_machine_name
-          ssh_keys_to_save=sshkeys.reject{|k,v| k == curr_machine_name}.values.join.gsub("\n\n", '\n')
-          curr_machine.communicate.execute("echo '"+ssh_keys_to_save+"' >/tmp/.all_keys.txt")
-          #puts "Saved /tmp/.all_keys.txt"
+					curr_machine_name=curr_machine.name.to_s
+					puts "Getting SSH keys from "+curr_machine_name
 
-          ssh_root_keys_to_save=sshrootkeys.reject{|k,v| k == curr_machine_name}.values.join.gsub("\n\n", '\n')
-          curr_machine.communicate.execute("echo '"+ssh_root_keys_to_save+"' >/tmp/.all_root_keys.txt")
+					required_users.each do |curr_user|
+						if !check_user_existence(curr_machine, curr_user)
+							create_user(curr_machine, curr_user)
+						end
 
-          #puts "Saved /tmp/.all_root_keys.txt"
+						if !sshkeys[curr_user]
+							sshkeys[curr_user] = Hash.new
+						end
+						sshkey=get_user_key(curr_machine, curr_user)
+						#puts "SSH key for "+curr_user+": "+sshkey
+						sshkeys[curr_user][curr_machine_name] = sshkey
+					end
+				end
 
-          # We must save locally a bash script that computes and applies diff and always exits with 0 or vagrant plugin will exit with an error
-          curr_machine.communicate.execute("echo 'diff --changed-group-format=\"%>\" --unchanged-group-format=\"\" ~/.ssh/authorized_keys $1 >>~/.ssh/authorized_keys;exit 0' >/tmp/add_ssh_keys.sh")
+				#pp sshkeys
 
-          curr_machine.communicate.execute("sh /tmp/add_ssh_keys.sh /tmp/.all_keys.txt")
-          curr_machine.communicate.execute("sh /tmp/add_ssh_keys.sh /tmp/.all_root_keys.txt")
-          #puts "Saved user keys"
-          
-          curr_machine.communicate.sudo("sh /tmp/add_ssh_keys.sh /tmp/.all_keys.txt")
-          curr_machine.communicate.sudo("sh /tmp/add_ssh_keys.sh /tmp/.all_root_keys.txt")
-          #puts "Saved root keys"
-        end
+				running_machines.each do |curr_machine|
+					curr_machine_name=curr_machine.name.to_s
 
-        machines.each do |curr_machine|
-          call_extra_user_steps(curr_machine)
-        end
+					puts "Saving public SSH keys to "+curr_machine_name
 
-        machines.each do |curr_machine|
-          curr_machine.communicate.sudo("rm -f /tmp/.all_keys.txt /tmp/.all_root_keys.txt /tmp/add_ssh_keys.sh")
-        end
-      end
+					required_users.each do |curr_user|
+						curr_machine.communicate.sudo("rm -f /tmp/.all_"+curr_user+"_keys.txt")
 
-      private
+						ssh_keys_to_save=sshkeys[curr_user].reject{|k,v| k == curr_machine_name}.values.join.gsub("\n\n", '\n')
+						curr_machine.communicate.execute("sudo -u "+curr_user+" -H echo '"+ssh_keys_to_save+"' >/tmp/.all_"+curr_user+"_keys.txt")
+						#puts "Saved /tmp/.all_"+curr_user+"_keys.txt"
+					end
 
-      def create_user_if_missing(username)
-        machine.communicate.sudo("if [ ! $(id -u recom 2>/dev/null) ]; then adduser "+username+"; fi")
-      end
+					required_users.each do |curr_user|
+						required_users.each do |source_user|
+							curr_machine.communicate.execute("sudo -u "+curr_user+" -H sh /tmp/add_ssh_keys.sh /tmp/.all_"+source_user+"_keys.txt")
+						end
+						#puts "Saved user "+curr_user+"keys"
+					end
+				end
 
-      def get_user_key(machine)
-        sshresult=""
-        machine.communicate.execute("if [ ! -e ~/.ssh/id_rsa ] || [ ! -e ~/.ssh/id_rsa.pub ]; then ssh-keygen -q -f ~/.ssh/id_rsa -P ''; fi; cat ~/.ssh/id_rsa.pub") do |type, data|
-          sshresult << data if type == :stdout
-        end
-        return sshresult
-      end
+				running_machines.each do |curr_machine|
+					call_extra_user_steps(curr_machine)
+				end
 
-      def get_root_key(machine)
-        sshrootresult = ""
-        machine.communicate.sudo("if [ ! -e ~/.ssh/id_rsa ] || [ ! -e ~/.ssh/id_rsa.pub ]; then ssh-keygen -q -f ~/.ssh/id_rsa -P ''; fi; cat ~/.ssh/id_rsa.pub") do |type, data|
-          sshrootresult << data if type == :stdout
-        end
-        return sshrootresult
-      end
+				running_machines.each do |curr_machine|
+					curr_machine.communicate.sudo("rm -f /tmp/add_ssh_keys.sh /tmp/get_user_keys.sh")
+					required_users.each do |curr_user|
+						curr_machine.communicate.sudo("rm -f /tmp/.all_"+curr_user+"_keys.txt")
+					end
+				end
+			end
 
-      def call_extra_user_steps(resolving_machine)
-        extra_user_steps = machine.config.keymanager.extra_steps
-        if extra_user_steps
-          machines = @global_env.machine_names
-          machines.map { |machine| extra_user_steps.call(machine, resolving_machine) }
-        end
-      end
+			private
 
-      def get_machines
-        machines = @global_env.machine_names
-        # Collect only machines that exist for the current provider
-        machines.collect do |name|
-              begin
-                machine = @global_env.machine(name, @provider)
-              rescue Vagrant::Errors::MachineNotFound
-                # ignore
-              end
-              machine
-            end
-          .reject(&:nil?)
-      end      
+			def check_user_existence(machine, username)
+				if username != "root"
+					user_id = ""
+					machine.communicate.execute("id -u "+username+" 2>/dev/null; exit 0") do |type, data|
+						user_id << data if type == :stdout
+					end
+					return user_id != ""
+				else
+					return true
+				end
+			end
 
-    end
-  end
+			def create_user(machine, username)
+				puts "Creating user "+username+" on "+machine.name.to_s
+				machine.communicate.sudo("adduser "+username)
+			end
+
+			def get_user_key(machine, username)
+				sshresult=""
+				machine.communicate.execute("sudo -u "+username+" -H sh /tmp/get_user_keys.sh") do |type, data|
+					sshresult << data if type == :stdout
+				end
+				return sshresult
+			end
+
+			def call_extra_user_steps(resolving_machine)
+				extra_user_steps = machine.config.keymanager.extra_steps
+				if extra_user_steps
+					machines = @global_env.machine_names
+					machines.map { |machine| extra_user_steps.call(machine, resolving_machine) }
+				end
+			end
+
+			def get_machines
+				machines = @global_env.machine_names
+				# Collect only machines that exist for the current provider
+				machines.collect do |name|
+							begin
+								machine = @global_env.machine(name, @provider)
+							rescue Vagrant::Errors::MachineNotFound
+								# ignore
+							end
+							machine
+						end
+					.reject(&:nil?)
+			end      
+
+		end
+	end
 end
